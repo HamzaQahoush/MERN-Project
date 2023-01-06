ef4dc45030a6b5bb3d3027d3a1024cbe740f6abe
Hamza Qahoush <hamza.qah@gmail.com>
2023-01-06 21:33:22 +0300

    connecting user-getUser api with front-end

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -1,15 +1,47 @@
-import React from "react";
-import UserList from "../components/UsersList";
+import React, { useState, useEffect } from 'react';
+import UserList from '../components/UsersList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 function Users() {
-  const Users = [
-    {
-      id: 'u1',
-      name: "Hamza Qahoush",
-      image: "https://avatars.githubusercontent.com/u/79087330?v=4",
-      places: 3,
-    },
-  ];
-  return <UserList items={Users} />;
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      let response;
+      try {
+        setIsLoading(true);
+        response = await fetch('http://localhost:5000/api/users');
+        const json = await response.json();
+        await setUsers(json.users);
+      } catch (err) {
+        setError(err.message);
+      }
+
+      if (!response.ok) {
+        throw new Error(
+          response.message || 'something went wroong please try agian later'
+        );
+      }
+    };
+
+    fetchData();
+    setIsLoading(false);
+  }, []);
+  const errorHandler = () => {
+    setError(null);
+  };
+  return (
+    <>
+      <ErrorModal onClear={errorHandler} error={error} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {users && !isLoading && <UserList items={users} />}
+    </>
+  );
 }
 
 export default Users;
